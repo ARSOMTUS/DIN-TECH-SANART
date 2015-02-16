7931aec9d48ed34e1ded9bb8aea71ab8deb3f027
emorozov <morozovei91@gmail.com>
2015-02-16 22:15:00 +0400

    js, js, js >.<

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,38 @@ window.onload = function() {
         var subLayer = document.getElementById('sub_layer');
         var wrappFormFeedback = document.getElementById('wrapp_form_feedback');
         
+        var emailDinTech = document.getElementById('e_mail_din_tech');
+        var btnGetFreeSample = document.getElementById('btn_get_free_sample');
+        
+        var btnClose = document.getElementById('btn_close');
+        
+        var showForm = function(){
+            
+            subLayer.style.display = 'block';
+            wrappFormFeedback.style.display = 'block';
+        };
+        
+        var closeForm = function(){
+            
+            var rConfirm = confirm("Закрыть окно? Введённые данные будут утеряны.");
+            
+            if (rConfirm === true){
+                
+                subLayer.style.display = 'none';
+                wrappFormFeedback.style.display = 'none';
+                document.getElementById("form_feedback").reset();
+            }
+            else{}
+        };
+        
+        btnGetFreeSample.addEventListener('click', showForm);
+        emailDinTech.addEventListener('click', showForm);
+        
+        subLayer.addEventListener('click',closeForm);
+        btnClose.addEventListener('click',closeForm);
+        
+        
+
 //        var btnHideMoreInfo = document.getElementById('hide_more_info_btn');
 //        var blockMoreInfo = document.getElementById('more_info_block');
 //        var btnsHideMoreInfo = document.getElementsByClassName('hide-more-info-btn');
